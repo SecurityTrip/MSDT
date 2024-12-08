2d03ae73376aeae06f22f4b2e1da3fd5111c64fc
Илья Лысов <illigiorno@gmail.com>
2024-12-08 21:12:53 +0400

    Add logging

--- msdt-4/TestTask-master/Classes.py
+++ msdt-4/TestTask-master/Classes.py
@@ -1,5 +1,12 @@
 import time
+import logging
 
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()]
+)
 
 class Singleton(object):
     def __new__(cls, *args, **kw):
@@ -12,192 +19,95 @@ class Singleton(object):
 class SmallElevator(Singleton):  # Пассажирский лифт
     MAXWEIGHT = 400
     MAXPEOPLE = 5
-
-
     alavlable = True
-
     SPEED = 2  # m/s
-
     FLOORHEIGHT = 2.7  # meters
-
     CurretFloor = 1
-
     status = ["Едет вверх", "Едет вниз", "Открывает двери", "Закрывает двери", "Стоит с открытыми дверьми",
               "Вызов диспетчера"]
     CurrentStatusIndex = 4
-
     doorStatus = "Открыты"
 
     def GetStatus(self):
         return self.status[self.CurrentStatusIndex]
 
     def pressButtonFloor(self, floor):
-        #if 1 >= floor <= 20:
-            self.RunToFloor(floor)
+        self.RunToFloor(floor)
 
     def pressButtonCloseDoors(self):
-        self.doorStatus = "Открыты"
+        self.doorStatus = "Закрыты"
+        logging.info("Двери лифта закрыты")
 
     def pressButtonOpenDoors(self):
-        self.doorStatus = "Закрыты"
+        self.doorStatus = "Открыты"
+        logging.info("Двери лифта открыты")
 
     def pressButtonCallDispatcher(self):
         self.CurrentStatusIndex = 5
-        print(self.status[self.CurrentStatusIndex])
-        print("Идёт вызов диспетчера")
+        logging.warning(f"{self.status[self.CurrentStatusIndex]}: Идёт вызов диспетчера")
 
     def RunToFloor(self, floor):
         if floor > self.CurretFloor:
             self.CurrentStatusIndex = 3
-            print(self.status[self.CurrentStatusIndex])
+            logging.info(self.status[self.CurrentStatusIndex])
             self.doorStatus = "Закрыты"
 
             self.CurrentStatusIndex = 0
-            print(self.status[self.CurrentStatusIndex])
-
-            print(f"Пассажирский лифт едет на {floor} этаж")
+            logging.info(self.status[self.CurrentStatusIndex])
+            logging.info(f"Пассажирский лифт едет на {floor} этаж")
 
             meters = (floor - self.CurretFloor + 1) * self.FLOORHEIGHT
             time.sleep(meters / self.SPEED)
 
             self.CurretFloor = floor
-            print(f"Пассажирский лифт приехал на {self.CurretFloor} этаж")
+            logging.info(f"Пассажирский лифт приехал на {self.CurretFloor} этаж")
 
             self.CurrentStatusIndex = 2
-            print(self.status[self.CurrentStatusIndex])
+            logging.info(self.status[self.CurrentStatusIndex])
             self.doorStatus = "Открыты"
 
         elif floor < self.CurretFloor:
             self.CurrentStatusIndex = 3
-            print(self.status[self.CurrentStatusIndex])
+            logging.info(self.status[self.CurrentStatusIndex])
             self.doorStatus = "Закрыты"
 
             self.CurrentStatusIndex = 1
-            print(self.status[self.CurrentStatusIndex])
-
-            print(f"Пассажирский лифт едет на {floor} этаж")
+            logging.info(self.status[self.CurrentStatusIndex])
+            logging.info(f"Пассажирский лифт едет на {floor} этаж")
 
             meters = (self.CurretFloor - floor + 1) * self.FLOORHEIGHT
             time.sleep(meters / self.SPEED)
 
             self.CurretFloor = floor
-            print(f"Пассажирский лифт приехал на {self.CurretFloor} этаж")
+            logging.info(f"Пассажирский лифт приехал на {self.CurretFloor} этаж")
 
             self.CurrentStatusIndex = 2
-            print(self.status[self.CurrentStatusIndex])
+            logging.info(self.status[self.CurrentStatusIndex])
             self.doorStatus = "Открыты"
 
-        elif floor == self.CurretFloor:
-            print(f"Пассажирский лифт уже на {self.CurretFloor} этаже")
+        else:
+            logging.info(f"Пассажирский лифт уже на {self.CurretFloor} этаже")
             self.doorStatus = "Открыты"
             self.CurrentStatusIndex = 4
-            print(self.status[self.CurrentStatusIndex])
+            logging.info(self.status[self.CurrentStatusIndex])
 
-
-class BigElevator(Singleton):  # Грусзовой лифт
+# Аналогично обновляем BigElevator, меняя сообщения на соответствующие
+class BigElevator(SmallElevator):  # Грузовой лифт
     MAXWEIGHT = 800
     MAXPEOPLE = 10
-
-    alavlable = True
-
     SPEED = 1.8  # m/s
 
-    FLOORHEIGHT = 2.7  # meters
-
-    CurretFloor = 1
-
-    status = ["Едет вверх", "Едет вниз", "Открывает двери", "Закрывает двери", "Стоит с открытыми дверьми",
-              "Вызов диспетчера"]
-    CurrentStatusIndex = 4
-
-    doorStatus = "Открыты"
-
-    def GetStatus(self):
-        return self.status[self.CurrentStatusIndex]
-
-    def pressButtonFloor(self, floor):
-        #if 1 >= floor <= 20:
-            self.RunToFloor(floor)
-
-    def pressButtonCloseDoors(self):
-        self.doorStatus = "Открыты"
-
-    def pressButtonOpenDoors(self):
-        self.doorStatus = "Закрыты"
-
-    def pressButtonCallDispatcher(self):
-        self.CurrentStatusIndex = 5
-        print(self.status[self.CurrentStatusIndex])
-        print("Идёт вызов диспетчера")
-
-    def RunToFloor(self, floor):
-        if floor > self.CurretFloor:
-            self.CurrentStatusIndex = 3
-            print(self.status[self.CurrentStatusIndex])
-            self.doorStatus = "Закрыты"
-
-            self.CurrentStatusIndex = 0
-            print(self.status[self.CurrentStatusIndex])
-
-            print(f"Грузовой лифт едет на {floor} этаж")
-
-            meters = (floor - self.CurretFloor + 1) * self.FLOORHEIGHT
-            time.sleep(meters / self.SPEED)
-
-            self.CurretFloor = floor
-            print(f"Грузовой лифт приехал на {self.CurretFloor} этаж")
-
-            self.CurrentStatusIndex = 2
-            print(self.status[self.CurrentStatusIndex])
-            self.doorStatus = "Открыты"
-
-
-        elif floor < self.CurretFloor:
-            self.CurrentStatusIndex = 3
-            print(self.status[self.CurrentStatusIndex])
-            self.doorStatus = "Закрыты"
-
-            self.CurrentStatusIndex = 1
-            print(self.status[self.CurrentStatusIndex])
-
-            print(f"Грузовой лифт едет на {floor} этаж")
-
-            meters = (self.CurretFloor - floor + 1) * self.FLOORHEIGHT
-            time.sleep(meters / self.SPEED)
-
-            self.CurretFloor = floor
-            print(f"Грузовой лифт приехал на {self.CurretFloor} этаж")
-
-            self.CurrentStatusIndex = 2
-            print(self.status[self.CurrentStatusIndex])
-            self.doorStatus = "Открыты"
-            pass
-
-        elif floor == self.CurretFloor:
-            print(f"Грузовой лифт уже на {self.CurretFloor} этаже")
-            self.doorStatus = "Открыты"
-            self.CurrentStatusIndex = 4
-            print(self.status[self.CurrentStatusIndex])
-
-
+# Обновляем класс Floor
 class Floor:
     BigElevatorClass, SmallElevatorClass = BigElevator(), SmallElevator()
-
     THISFLOOR = 0
 
     def __init__(self, a):
         self.THISFLOOR = a
 
-    BigEvevatorFloor = 1
-    BigEvevatorStatus = BigElevatorClass.GetStatus()
-
-    SmallEvevatorFloor = 1
-    SmallEvevatorStatus = SmallElevatorClass.GetStatus()
-
     def CallElevator(self):
-        print(f"Лифт вызван на {self.THISFLOOR} этаж")
-        if (abs(self.BigElevatorClass.CurretFloor - self.THISFLOOR)) < (
-                abs(self.SmallElevatorClass.CurretFloor - self.THISFLOOR)):
+        logging.info(f"Лифт вызван на {self.THISFLOOR} этаж")
+        if abs(self.BigElevatorClass.CurretFloor - self.THISFLOOR) < abs(self.SmallElevatorClass.CurretFloor - self.THISFLOOR):
             self.BigElevatorClass.RunToFloor(self.THISFLOOR)
         else:
             self.SmallElevatorClass.RunToFloor(self.THISFLOOR)
--- msdt-4/TestTask-master/main.py
+++ msdt-4/TestTask-master/main.py
@@ -1,68 +1,45 @@
 import random
-
+import logging
 from Classes import Floor, SmallElevator, BigElevator
 
-floors = []
-for i in range(1, 21):
-    floors.append(Floor(i))
+logging.basicConfig(level=logging.INFO)
 
+floors = [Floor(i) for i in range(1, 21)]
 BE = BigElevator()
 SE = SmallElevator()
 
-
 def simulation():
+    logging.info("Запуск симуляции")
     floors[0].CallElevator()
-    tmp = random.randrange(1, 2, 1)
-    if tmp == 1:
+    if random.choice([True, False]):
         SE.pressButtonFloor(14)
     else:
         BE.pressButtonFloor(14)
 
-    # print(f"Грузовой лифт на {BE.CurretFloor} этаже")
-    # print(f"Пассажирский лифт на {SE.CurretFloor} этаже")
-
     floors[14].CallElevator()
-
     if BE.CurretFloor == 15:
         BE.pressButtonFloor(1)
     if SE.CurretFloor == 15:
         SE.pressButtonFloor(1)
 
-
 def handSim(floors):
-    stop = True
     while True:
-        tmpBool = str(input("Начать ручную симуляцию работы лифтов? (Y/N)")).upper()
+        tmpBool = input("Начать ручную симуляцию работы лифтов? (Y/N): ").strip().upper()
         if tmpBool == "Y":
-            stop = False
-            break
-        if tmpBool == "N":
-            stop = True
-            break
-
-    while(not stop):
-        floor = int(input("Введите на каком вы этаже: "))
-        floors[floor-1].CallElevator()
-
-        while(True):
-            if floors[floor-1].THISFLOOR == floors[floor-1].SmallElevatorClass.CurretFloor:
+            while True:
+                floor = int(input("Введите на каком вы этаже: "))
+                floors[floor-1].CallElevator()
                 newfloor = int(input("На какой этаж едем? "))
-                floors[floor - 1].SmallElevatorClass.pressButtonFloor(newfloor)
-                break
-            if floors[floor-1].THISFLOOR == floors[floor-1].BigElevatorClass.CurretFloor:
-                newfloor = int(input("На какой этаж едем? "))
-                floors[floor - 1].BigElevatorClass.pressButtonFloor(newfloor)
-                break
+                if floors[floor-1].THISFLOOR == floors[floor-1].SmallElevatorClass.CurretFloor:
+                    floors[floor - 1].SmallElevatorClass.pressButtonFloor(newfloor)
+                elif floors[floor-1].THISFLOOR == floors[floor-1].BigElevatorClass.CurretFloor:
+                    floors[floor - 1].BigElevatorClass.pressButtonFloor(newfloor)
 
-
-        print(f"Лифт приехал на {floor} этаж")
-        tmpBool = str(input("Продолжить симуляцию? (Y/N)")).upper()
-        if tmpBool == "Y":
-            stop = False
-        if tmpBool == "N":
-            stop = True
-
-
+                tmpBool = input("Продолжить симуляцию? (Y/N): ").strip().upper()
+                if tmpBool == "N":
+                    break
+        elif tmpBool == "N":
+            break
 
 if __name__ == "__main__":
     simulation()
